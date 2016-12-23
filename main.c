bd2f2862d943a470fe60ddfd89a687cdae90eb9c
Ben Campbell <ben@scumways.com>
2016-12-23 23:39:33 +1300

    first-draft sdl view

--- main.c
+++ main.c
@@ -1,22 +1,112 @@
-// gcc -ggdb im.c im_png.c main.c `pkg-config libpng --libs --cflags`
+// gcc -ggdb im.c im_png.c main.c `pkg-config libpng sdl2 --libs --cflags`
 
 #include "im.h"
 #include <stdio.h>
 
+#include <SDL.h>
+#include <SDL_surface.h>
+
+SDL_Window *win = NULL;
+SDL_Renderer *ren = NULL;
+im_Img* img = NULL;
+SDL_Surface *bmp = NULL;
+SDL_Texture *tex = NULL;
+
+void cleanup();
 
 int main( int argc, char* argv[])
 {
-    if (argc>1) {
-        im_Img *img = loadPng(argv[1]);
-        if (img==NULL) {
-            printf("Poop.\n");
-            return 1;
-        }
-
-
-        printf("%dx%d\n", img->Width, img->Height);
+    if (argc<=1) {
+        return 0;
     }
 
+    if(SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+        fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (!win) {
+        fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+        cleanup();
+	    return 1;
+    }
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == 0){
+        fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+        cleanup();
+    	return 1;
+    }
+
+    im_Img *img = loadPng(argv[1]);
+    if (img==NULL) {
+        fprintf(stderr,"Poop.\n");
+        cleanup();
+        return 1;
+    }
+
+/*    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+    if (bmp == nullptr){
+        fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+        cleanup();
+        return 1;
+    }
+*/
+
+    // bmp shares the pixel data (it doesn't take it's own copy)
+    bmp = SDL_CreateRGBSurfaceFrom(img->Data,
+            img->Width, img->Height, 24, img->Pitch, 0xff0000,0x00ff00,0x0000ff,0);
+    if (!bmp) {
+        fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+        cleanup();
+        return 1;
+    }
+    
+
+    printf("%dx%d\n", img->Width, img->Height);
+
+    tex = SDL_CreateTextureFromSurface(ren, bmp);
+    if (tex == 0 ){
+        cleanup();
+    	return 1;
+    }
+    SDL_FreeSurface(bmp);
+    bmp=NULL;
+
+
+for (int i = 0; i < 3; ++i){
+	//First clear the renderer
+	SDL_RenderClear(ren);
+	//Draw the texture
+	SDL_RenderCopy(ren, tex, NULL, NULL);
+	//Update the screen
+	SDL_RenderPresent(ren);
+	//Take a quick break after all that hard work
+	SDL_Delay(1000);
+}
+    cleanup();
+    return 0;
+}
+
+void cleanup()
+{
+    if (tex) {
+        SDL_DestroyTexture(tex);
+    }
+    if (bmp) {
+        SDL_FreeSurface(bmp);
+    }
+    if (img) {
+        im_img_free(img);
+    }
+    if (ren) {
+        SDL_DestroyRenderer(ren);
+    }
+    if (win) {
+        SDL_DestroyWindow(win);
+    }
+    SDL_Quit();
 }
 /*
 
