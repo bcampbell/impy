02c27e51072999dcb385101703249cefd0d3387c
Ben Campbell <ben@scumways.com>
2016-12-25 23:01:35 +1300

    sdl viewer broken - fiddling

--- main.c
+++ main.c
@@ -11,16 +11,19 @@ SDL_Renderer *ren = NULL;
 im_Img* img = NULL;
 SDL_Surface *bmp = NULL;
 SDL_Texture *tex = NULL;
+int scale = 1;
 
 void cleanup();
+void show();
 
 int main( int argc, char* argv[])
 {
+    int quit = 0;
     if (argc<=1) {
         return 0;
     }
 
-    if(SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0 ) {
         fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
         return 1;
     }
@@ -32,7 +35,7 @@ int main( int argc, char* argv[])
 	    return 1;
     }
 
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 0); //SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (ren == 0){
         fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
         cleanup();
@@ -71,23 +74,69 @@ int main( int argc, char* argv[])
         cleanup();
     	return 1;
     }
-    SDL_FreeSurface(bmp);
-    bmp=NULL;
 
+    show();
+    while (!quit) {
+        SDL_Event ev;
+        if (!SDL_WaitEvent(&ev)) {
+            fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+            cleanup();
+            return 1;
+        }
+        switch (ev.type) {
+            case SDL_QUIT:
+            {
+                quit = 1;
+                break;
+            }
+            case SDL_KEYDOWN:
+            {
+                switch (ev.key.keysym.scancode) {
+                    case SDL_SCANCODE_ESCAPE: quit = 1; break;
+                    case SDL_SCANCODE_1: scale = 1; break;
+                    case SDL_SCANCODE_2: scale = 2; break;
+                    case SDL_SCANCODE_3: scale = 4; break;
+                    case SDL_SCANCODE_4: scale = 8; break;
+                    case SDL_SCANCODE_5: scale = 16; break;
+                }
+                show();
+                break;
+            }
+        }
+    }
 
-for (int i = 0; i < 3; ++i){
-	//First clear the renderer
-	SDL_RenderClear(ren);
-	//Draw the texture
-	SDL_RenderCopy(ren, tex, NULL, NULL);
-	//Update the screen
-	SDL_RenderPresent(ren);
-	//Take a quick break after all that hard work
-	SDL_Delay(1000);
-}
     cleanup();
     return 0;
 }
+
+
+void show()
+{
+    int winw, winh;
+    int texw, texh;
+    SDL_Rect src, dest;
+/*
+    SDL_GetWindowSize(win, &winw, &winh);
+
+    if (SDL_QueryTexture(tex,NULL,NULL,&texw, &texh) != 0 ) {
+        fprintf(stderr, "ERROR: %s\n", SDL_GetError() );
+        return;
+    }
+
+    printf("win: %dx%d\n",winw,winh);
+*/
+	SDL_RenderClear(ren);
+    dest.w = texw*scale;
+    dest.h = texh*scale;
+    dest.x = 0; //(winw-dest.w)/2;
+    dest.y = 0; //(winh-dest.h)/2;
+	SDL_RenderCopy(ren, tex, NULL, NULL);
+	SDL_RenderPresent(ren);
+    
+}
+
+
+
 
 void cleanup()
 {
