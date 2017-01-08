1d57265310ed789fbac0ebf2d5460a05ee70fbdc
Ben Campbell <ben@scumways.com>
2017-01-08 17:00:36 +1300

    split default im_img implementation into own file.

--- im.c
+++ im.c
@@ -11,7 +11,6 @@ static struct handler *handlers[] = { &handle_png, &handle_gif, NULL };
 
 
 static struct handler* pick_handler_for_read(im_reader* rdr);
-static void copy_pal_range( ImPalFmt src_fmt, const uint8_t* src, ImPalFmt dest_fmt, uint8_t *dest, int first_colour, int num_colours);
 
 void* imalloc( size_t size)
 {
@@ -60,28 +59,6 @@ static int bpp(ImFmt fmt, ImDatatype datatype) {
     return 0;
 }
 
-
-// returns true if palettes are equal (same format, same colours)
-bool im_img_pal_equal(const im_img* a, const im_img* b)
-{
-    size_t nbytes;
-
-    if (im_img_pal_num_colours(a) != im_img_pal_num_colours(b)) {
-        return false;
-    }
-    if (im_img_pal_fmt(a) != im_img_pal_fmt(b)) {
-        return false;
-    }
-    switch (im_img_pal_fmt(a)) {
-        case PALFMT_RGB: nbytes=3; break;
-        case PALFMT_RGBA: nbytes=4; break;
-        default: return false;
-    }
-    if(memcmp( im_img_pal_data(a), im_img_pal_data(b), nbytes) != 0 ) {
-        return false;
-    }
-    return true;
-}
 
 
 static struct handler* pick_handler_by_filename(const char* filename)
@@ -240,61 +217,3 @@ bool im_bundle_save( im_bundle* bundle, const char* filename, ImErr* err )
     return result;
 }
 
-
-// load colours into palette
-bool im_img_pal_write( im_img* img, int first_colour, int num_colours, ImPalFmt data_fmt, const void* data)
-{
-    if (first_colour+num_colours > im_img_pal_num_colours(img)) {
-        return false;
-    }
-
-    copy_pal_range(
-        data_fmt, data,
-        im_img_pal_fmt(img), im_img_pal_data(img),
-        first_colour, num_colours);
-    return true;
-}
-
-
-// read colours from palette
-bool im_img_pal_read( im_img* img, int first_colour, int num_colours, ImPalFmt dest_fmt, void* dest)
-{
-    if (first_colour+num_colours > im_img_pal_num_colours(img)) {
-        return false;
-    }
-
-    copy_pal_range(
-        im_img_pal_fmt(img), im_img_pal_data(img),
-        dest_fmt, dest,
-        first_colour, num_colours);
-    return true;
-}
-
-
-// helper func - copy/convert palette colours
-static void copy_pal_range( ImPalFmt src_fmt, const uint8_t* src, ImPalFmt dest_fmt, uint8_t *dest, int first_colour, int num_colours) {
-
-    if (src_fmt==PALFMT_RGB && dest_fmt==PALFMT_RGB) {
-        memcpy(dest + (first_colour*3), src, num_colours*3);
-    } else if (src_fmt==PALFMT_RGBA && dest_fmt==PALFMT_RGBA) {
-        memcpy(dest + (first_colour*4), src, num_colours*4);
-    } else if (src_fmt == PALFMT_RGB && dest_fmt==PALFMT_RGBA) {
-        int i;
-        dest += 4*first_colour;
-        for (i=0; i<num_colours; ++i) {
-            *dest++ = *src++;
-            *dest++ = *src++;
-            *dest++ = *src++;
-            *dest++ = 255;
-        }
-    } else if (src_fmt == PALFMT_RGBA && dest_fmt==PALFMT_RGB) {
-        int i;
-        dest += 3*first_colour;
-        for (i=0; i<num_colours; ++i) {
-            *dest++ = *src++;
-            *dest++ = *src++;
-            *dest++ = *src++;
-            ++src;
-        }
-    }
-}
--- img.c
+++ img.c
@@ -1,236 +1,95 @@
+// implementation-neutral support for im_img
+
 #include "im.h"
 #include "private.h"
 
-#include <string.h>
+#include <string.h>    // for memcpy
 
 
+static void copy_pal_range( ImPalFmt src_fmt, const uint8_t* src, ImPalFmt dest_fmt, uint8_t *dest, int first_colour, int num_colours);
 
 
-// default im_img implementation
-
-struct image {
-    int width;
-    int height;
-    int depth;
-
-    ImFmt format;     // FMT_*
-    ImDatatype datatype;     // DT_*
-
-    int pitch;  // bytes per line
-    void* pixel_data;
-
-    // palette
-    int pal_num_colours;    // 0=no palette
-    ImPalFmt pal_fmt;
-    void* pal_data;     // can be NULL, iff num_colours==0
-
-    // metadata
-    int x_offset;
-    int y_offset;
-    // TODO: disposal, frame duration, etc...
-};
+// the current im_img implementation
+im_img_impl* im_current_img_impl = &im_default_img_impl;
 
 
-
-// calc bytes per pixel
-static int bpp(ImFmt fmt, ImDatatype datatype) {
-    int n=0;
-
-    switch (datatype) {
-        case DT_U8:
-        case DT_S8:
-            n=1;
-            break;
-        case DT_U16:
-        case DT_S16:
-        case DT_FLOAT16:
-            n=2;
-            break;
-        case DT_U32:
-        case DT_S32:
-        case DT_FLOAT32:
-            n=4;
-            break;
-        case DT_FLOAT64:
-            n=8;
-            break;
-    }
-
-    switch (fmt) {
-        case FMT_RGB: return 3*n;
-        case FMT_RGBA: return 4*n;
-        case FMT_BGR: return 3*n;
-        case FMT_BGRA: return 4*n;
-        case FMT_COLOUR_INDEX: return 1*n;
-        case FMT_ALPHA: return 1*n;
-        case FMT_LUMINANCE: return 1*n;
-    }
-    return 0;
-}
-
-
-im_img* image_create(int w, int h, int d, ImFmt fmt, ImDatatype datatype)
+bool im_img_pal_equal(const im_img* a, const im_img* b)
 {
-    struct image* foo;
-    int bytesPerPixel;
-    int datsize;
+    size_t nbytes;
 
-    if( w<1 || h<1 || d<1 ) {
-        return NULL;
+    if (im_img_pal_num_colours(a) != im_img_pal_num_colours(b)) {
+        return false;
     }
-
-    bytesPerPixel = bpp(fmt,datatype);
-    if(bytesPerPixel==0) {
-        return NULL;
+    if (im_img_pal_fmt(a) != im_img_pal_fmt(b)) {
+        return false;
     }
-
-    foo = imalloc(sizeof(struct image));
-    if (foo==NULL) {
-        return 0;
-    }
-
-    foo->width = w;
-    foo->height = h;
-    foo->depth = d;
-
-
-    foo->format = fmt;
-    foo->datatype = datatype;
-    foo->pitch = bytesPerPixel * foo->width;
-    foo->pixel_data = imalloc(foo->height * foo->pitch * foo->depth);
-    if (!foo->pixel_data) {
-        ifree(foo);
-        return NULL;
-    }
-
-    foo->pal_num_colours = 0;
-    foo->pal_fmt = PALFMT_RGB;
-    foo->pal_data = 0;
-
-    foo->x_offset = 0;
-    foo->y_offset = 0;
-
-    return (im_img*)foo;
-}
-
-void image_free(im_img *img)
-{
-    struct image* foo = (struct image*)img;
-    if (foo->pal_data) {
-        ifree(foo->pal_data);
-    }
-    ifree(foo);
-}
-
-
-static int image_w(const im_img *img)
-    { return ((const struct image*)img)->width; }
-
-static int image_h(const im_img *img)
-    { return ((const struct image*)img)->height; }
-
-static int image_d(const im_img *img)
-    { return ((const struct image*)img)->depth; }
-
-static ImFmt image_fmt(const im_img *img)
-    { return ((const struct image*)img)->format; }
-
-static ImDatatype image_datatype(const im_img *img)
-    { return ((const struct image*)img)->datatype; }
-
-static void* image_row(const im_img *img, int row)
-{
-    const struct image* foo = (const struct image*)img;
-    return ((uint8_t*)(foo->pixel_data)) + (row*foo->pitch);
-}
-
-static int image_pitch(const im_img *img)
-    { return ((const struct image*)img)->pitch; }
-
-// image palette fns
-static bool image_pal_set( im_img* img, ImPalFmt fmt, int ncolours, const void* data)
-{
-    struct image* foo = (struct image*)img;
-    int colsize;
-    switch (fmt) {
-        case PALFMT_RGB: colsize=3; break;
-        case PALFMT_RGBA: colsize=4; break;
+    switch (im_img_pal_fmt(a)) {
+        case PALFMT_RGB: nbytes=3; break;
+        case PALFMT_RGBA: nbytes=4; break;
         default: return false;
     }
-    if( foo->pal_fmt != fmt || foo->pal_num_colours != ncolours ) {
-        // reallocate
-        uint8_t* newdata = NULL;
-        if(ncolours > 0) {
-            newdata = imalloc(ncolours*colsize);
-            if (!newdata) {
-                return false;
-            }
-        }
-        if (foo->pal_data) {
-            ifree(foo->pal_data);
-        }
-        foo->pal_data = newdata;
-        foo->pal_fmt = fmt;
-        foo->pal_num_colours = ncolours;
-    }
-
-    if( data ) {
-        im_img_pal_write( img, 0, ncolours, fmt, data );
-    } else {
-        memset( foo->pal_data, 0, ncolours*colsize);
+    if(memcmp( im_img_pal_data(a), im_img_pal_data(b), nbytes) != 0 ) {
+        return false;
     }
     return true;
 }
 
 
-static void* image_pal_data(const im_img* img)
-    { return ((const struct image*)img)->pal_data; }
 
-static int image_pal_num_colours(const im_img* img)
-    { return ((const struct image*)img)->pal_num_colours; }
-
-static ImPalFmt image_pal_fmt(const im_img* img)
-    { return ((const struct image*)img)->pal_fmt; }
-
-// metadata access... too verbose?
-
-static int image_x_offset(const im_img *img)
-    { return ((const struct image*)img)->x_offset; }
-static int image_y_offset(const im_img *img)
-    { return ((const struct image*)img)->y_offset; }
-
-static void image_set_offset(im_img *img, int x, int y)
+// load colours into palette
+bool im_img_pal_write( im_img* img, int first_colour, int num_colours, ImPalFmt data_fmt, const void* data)
 {
-    struct image* foo = (struct image*)img;
-    foo->x_offset = x;
-    foo->y_offset = y;
+    if (first_colour+num_colours > im_img_pal_num_colours(img)) {
+        return false;
+    }
+
+    copy_pal_range(
+        data_fmt, data,
+        im_img_pal_fmt(img), im_img_pal_data(img),
+        first_colour, num_colours);
+    return true;
 }
 
-im_img_impl im_default_img_impl = {
-    image_create,
-    image_free,
-    image_w,
-    image_h,
-    image_d,
-    image_fmt,
-    image_datatype,
-    image_row,
-    image_pitch,
 
-    image_pal_set,
-    image_pal_data,
-    image_pal_num_colours,
-    image_pal_fmt,
+// read colours from palette
+bool im_img_pal_read( im_img* img, int first_colour, int num_colours, ImPalFmt dest_fmt, void* dest)
+{
+    if (first_colour+num_colours > im_img_pal_num_colours(img)) {
+        return false;
+    }
 
-    image_x_offset,
-    image_y_offset,
-    image_set_offset,
-};
+    copy_pal_range(
+        im_img_pal_fmt(img), im_img_pal_data(img),
+        dest_fmt, dest,
+        first_colour, num_colours);
+    return true;
+}
 
 
+// helper func - copy/convert palette colours
+static void copy_pal_range( ImPalFmt src_fmt, const uint8_t* src, ImPalFmt dest_fmt, uint8_t *dest, int first_colour, int num_colours) {
 
-
-im_img_impl* im_current_img_impl = &im_default_img_impl;
-
-
-
+    if (src_fmt==PALFMT_RGB && dest_fmt==PALFMT_RGB) {
+        memcpy(dest + (first_colour*3), src, num_colours*3);
+    } else if (src_fmt==PALFMT_RGBA && dest_fmt==PALFMT_RGBA) {
+        memcpy(dest + (first_colour*4), src, num_colours*4);
+    } else if (src_fmt == PALFMT_RGB && dest_fmt==PALFMT_RGBA) {
+        int i;
+        dest += 4*first_colour;
+        for (i=0; i<num_colours; ++i) {
+            *dest++ = *src++;
+            *dest++ = *src++;
+            *dest++ = *src++;
+            *dest++ = 255;
+        }
+    } else if (src_fmt == PALFMT_RGBA && dest_fmt==PALFMT_RGB) {
+        int i;
+        dest += 3*first_colour;
+        for (i=0; i<num_colours; ++i) {
+            *dest++ = *src++;
+            *dest++ = *src++;
+            *dest++ = *src++;
+            ++src;
+        }
+    }
+}
