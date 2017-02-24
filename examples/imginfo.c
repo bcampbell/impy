68d27e353d181b0e783b45b3b7082e5b8d210279
Ben Campbell <ben@scumways.com>
2017-02-24 15:53:43 +1300

    use cmake to build imginfo example

--- examples/imginfo.c
+++ examples/imginfo.c
@@ -6,6 +6,7 @@ int main( int argc, char* argv[])
 {
 im_bundle* bundle = NULL;
     ImErr err;
+    int n;
     if (argc<=1) {
         return 0;
     }
@@ -14,6 +15,18 @@ im_bundle* bundle = NULL;
     if (bundle==NULL) {
         fprintf(stderr,"load failed (err=%d)\n",err);
         return 1;
+    }
+
+    for (n=0; n<im_bundle_num_frames(bundle); ++n) {
+        im_img* img = im_bundle_get_frame(bundle,n);
+        printf("%d: ", n);
+        if (!img) {
+            printf("no image\n");
+        } else {
+            int w = im_img_w(img);
+            int h = im_img_h(img);
+            printf( "%dx%d\n", w,h );
+        }
     }
 
     im_bundle_free(bundle);
