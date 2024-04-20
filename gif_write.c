39babab60d5cd73b61ed08dc0198bb18d7c31a79
Ben Campbell <ben@scumways.com>
2024-04-20 16:25:17 +1200

    tidy up gif writing - looping and palettes.

--- gif_write.c
+++ gif_write.c
@@ -16,10 +16,7 @@ typedef struct gif_writer {
     // gif-specific stuff
     GifFileType *gif;
     ColorMapObject* global_cm;
-    int global_trans;
-    // Details about current frame
     ColorMapObject* local_cm;
-    int local_trans;
 } gif_writer;
 
 
@@ -35,7 +32,7 @@ static ColorMapObject* cvt_palette(unsigned int num_colours, const uint8_t *src,
 static ImErr translate_err(int gif_err_code);
 static int output_fn(GifFileType *gif, const GifByteType *buf, int size);
 
-//static bool is_power_of_two(uint32_t n) { return (n & (n - 1)) == 0; } 
+static bool write_loops(GifFileType*gif, int loops);
 
 
 static struct write_handler gif_write_handler = {
@@ -61,9 +58,7 @@ im_write* igif_new_writer(im_out* out, ImErr* err)
 
     gw->gif = NULL;
     gw->global_cm = NULL;
-    gw->global_trans = -1;
     gw->local_cm = NULL;
-    gw->local_trans = -1;
     return (im_write*)gw;
 }
 
@@ -96,7 +91,8 @@ static void emit_header(im_write* wr)
         }
 
         // Set up the global colormap (and transparent index)
-        gw->global_cm = cvt_palette(wr->pal_num_colours, wr->pal_data, &gw->global_trans);
+        int global_trans = NO_TRANSPARENT_COLOR;
+        gw->global_cm = cvt_palette(wr->pal_num_colours, wr->pal_data, &global_trans);
         if( gw->global_cm == NULL) {
             wr->err = IM_ERR_NO_PALETTE;
             return;
@@ -111,40 +107,34 @@ static void emit_header(im_write* wr)
             wr->w,  // TODO: option to specify screen size in advance?
             wr->h,
             calc_colour_res(gw->global_cm->ColorCount),
-            0,  // (bgcolor)
+            0,  // Background colour.
             gw->global_cm))
         {
             // failed
             wr->err = translate_err(gw->gif->Error);
             return;
         }
+
+        // Assume endlessly-looping gif.
+        write_loops(gw->gif, 0);
     }
 
     // Now, write out the frame!
 
-    // Need to pick palette and transparent index
-    // TODO: seems like we're _always_ writing out a palette for each frame...
-    // probably don't have to do that...
-    ColorMapObject* cm = gw->global_cm;
-    int trans = gw->global_trans;
-
-    // update the local palette (TODO: a better way to see if palette has changed!)
+    // Has the palette changed?
     if (gw->local_cm) {
         GifFreeMapObject(gw->local_cm);
     }
-    gw->local_cm = cvt_palette(wr->pal_num_colours, wr->pal_data, &gw->local_trans);
+    int trans = NO_TRANSPARENT_COLOR;
+    gw->local_cm = cvt_palette(wr->pal_num_colours, wr->pal_data, &trans);
     if( gw->local_cm == NULL) {
         wr->err = IM_ERR_NO_PALETTE;
         return;
     }
-
-    // If we've got a local palette and it's different to the global one,
-    // use it instead.
-    if (gw->local_cm) {
-        if (!colormaps_equal(cm, gw->local_cm)) {
-            cm = gw->local_cm;
-            trans = gw->local_trans;
-        }
+    if (colormaps_equal(gw->global_cm, gw->local_cm)) {
+        // No change. throw away the local cm.
+        GifFreeMapObject(gw->local_cm);
+        gw->local_cm = NULL;
     }
 
     // output a GCB block
@@ -154,7 +144,7 @@ static void emit_header(im_write* wr)
         gcb.DisposalMode = DISPOSAL_UNSPECIFIED;
         gcb.UserInputFlag = false;
         gcb.DelayTime = 10; // in 0.01sec units
-        gcb.TransparentColor = trans;   // TODO: local or global
+        gcb.TransparentColor = NO_TRANSPARENT_COLOR;    //trans;
 
         EGifGCBToExtension(&gcb, (GifByteType*)gcb_buf);
 
@@ -169,7 +159,7 @@ static void emit_header(im_write* wr)
         wr->x_offset, wr->y_offset,
         wr->w, wr->h,
         false,  // interlace
-        cm))        // TODO: can this be null?
+        gw->local_cm))        // can be null.
     {
         wr->err = translate_err(gw->gif->Error);
         return;
@@ -229,7 +219,7 @@ static ColorMapObject* cvt_palette(unsigned int num_colours, const uint8_t *src,
     if (!cm) {
         return NULL;
     }
-    *trans = -1;
+    *trans = NO_TRANSPARENT_COLOR;  // -1
 
     dest = cm->Colors;
     for (i=0; i<num_colours; ++i) {
@@ -239,7 +229,7 @@ static ColorMapObject* cvt_palette(unsigned int num_colours, const uint8_t *src,
         dest->Blue = *src++;
         ++dest;
         alpha = *src++;
-        if (*trans == -1 && alpha == 0) {
+        if (*trans == NO_TRANSPARENT_COLOR && alpha == 0) {
             *trans = i;
         }
     }
@@ -292,9 +282,6 @@ static void finish(im_write* wr)
 }
 
 
-
-// TODO: support gif looping!
-#if 0
 static bool write_loops(GifFileType*gif, int loops)
 {
     uint8_t blk1[11] = { 'N','E','T','S','C','A','P','E','2','.','0' };
@@ -313,5 +300,4 @@ static bool write_loops(GifFileType*gif, int loops)
     }
     return true;
 }
-#endif
 
